ee8ba7fc973e64bd550a4063c66f0ae7e8064e2a
NeoJhonn <jhonny.azevedo@gmail.com>
2024-06-15 18:32:21 -0300

    Adicionado autenticação simples

---
+++ src/main/java/br/com/jhonny_azevedo/spring_security/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.jhonny_azevedo.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
